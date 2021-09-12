316cddf29894ad6a52455d5ba9a6a2cfcbd01ff8
donorp <donorp@arhat.solutions>
2021-09-12 07:54:48 +0200

    chore: Add unionfs example, update api
    Signed-off-by: donorp <donorp@arhat.solutions>

---
+++ example_test.go
@@ -0,0 +1,47 @@
+package unionfs_test
+
+import (
+	"embed"
+	"io"
+	"io/fs"
+	"os"
+	"strconv"
+
+	"arhat.dev/unionfs"
+)
+
+var (
+	//go:embed trie
+	sampleEmbedFS embed.FS
+
+	sampleOSFS = os.DirFS("/some/rootfs")
+)
+
+func ExampleUnionFS() {
+	ufs := unionfs.New()
+	ufs.Map("/go/arhat.dev/unionfs/trie", "trie", sampleEmbedFS)
+	ufs.Map("/", "/some/rootfs", os.DirFS("/some/rootfs"))
+
+	f, err := ufs.Open("/go/arhat.dev/unionfs/trie/trie.go")
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = f.Close() }()
+
+	info, err := f.Stat()
+	if err != nil {
+		return
+	}
+	println("FILE trie.go NAME:", info.Name())
+
+	_, _ = io.ReadAll(f)
+
+	entries, err := fs.ReadDir(ufs, "/go/arhat.dev/unionfs/trie")
+	if err != nil {
+		panic(err)
+	}
+
+	for i, ent := range entries {
+		println("DIR trie/#"+strconv.Itoa(i)+" NAME:", ent.Name())
+	}
+}
