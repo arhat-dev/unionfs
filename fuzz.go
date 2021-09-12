2c6c51a8c2ed343a3634d7c24f354961894a7601
Jeffrey Stoke <me@arhat.dev>
2021-09-12 07:31:18 +0200

    chore: Disable fuzz test
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

--- fuzz.go
+++ fuzz.go
@@ -3,24 +3,24 @@
 
 package unionfs
 
-import (
-	"bytes"
-	"image/png"
-)
-
-// Fuzz example
-func Fuzz(data []byte) int {
-	img, err := png.Decode(bytes.NewReader(data))
-	if err != nil {
-		if img != nil {
-			panic("img != nil on error")
-		}
-		return 0
-	}
-	var w bytes.Buffer
-	err = png.Encode(&w, img)
-	if err != nil {
-		panic(err)
-	}
-	return 1
-}
+// import (
+// 	"bytes"
+// 	"image/png"
+// )
+//
+// // Fuzz example
+// func Fuzz(data []byte) int {
+// 	img, err := png.Decode(bytes.NewReader(data))
+// 	if err != nil {
+// 		if img != nil {
+// 			panic("img != nil on error")
+// 		}
+// 		return 0
+// 	}
+// 	var w bytes.Buffer
+// 	err = png.Encode(&w, img)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return 1
+// }
